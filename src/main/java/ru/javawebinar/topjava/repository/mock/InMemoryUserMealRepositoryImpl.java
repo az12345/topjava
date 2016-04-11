6cfd699936b3289cfccfa347df0e2ffbeb58666b
Nikita <ndrozdov87@gmail.com>
2016-04-11 17:54:07 +0300

    HW06

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserMealRepositoryImpl.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * GKislin
  * 15.09.2015.
  */
-public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
+public class InMemoryUserMealRepositoryImpl {
     private Map<Integer, UserMeal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -22,7 +22,6 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
         UserMealsUtil.MEAL_LIST.forEach(this::save);
     }
 
-    @Override
     public UserMeal save(UserMeal userMeal) {
         if (userMeal.isNew()) {
             userMeal.setId(counter.incrementAndGet());
@@ -31,25 +30,25 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
         return userMeal;
     }
 
-    @Override
     public void delete(int id) {
         repository.remove(id);
     }
 
-    @Override
+
     public UserMeal get(int id) {
         return repository.get(id);
     }
 
-    @Override
+
     public Collection<UserMeal> getAll() {
         return repository.values();
     }
 
 
-    @Override
     public UserMeal save(UserMeal userMeal, int userId) {
         return null;
     }
+
+
 }
 
